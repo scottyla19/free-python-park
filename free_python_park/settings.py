ff20fc99e2f8ef74c8531ef094183b2e56db6b7f
Scott <scottyla19@gmail.com>
2016-12-12 22:25:07 -0700

    Added google api image search. Still need to fix offset

--- free_python_park/settings.py
+++ free_python_park/settings.py
@@ -21,7 +21,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'h1mwp8-t&0iv5#x+=o*+#8hw7dr*dko70+itzy2cxc^#xt%xe_'
-
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+GOOGLE_API_CX = os.environ['GOOGLE_API_CX']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
